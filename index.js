94b8e8308d29c1cbaa592c23b0d5a722c661bab5
Harsh Jain <harshj2010@gmail.com>
2023-04-21 22:27:31 +0530

    Install ejs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const app=express();
 const port=8000;
+app.use(express.static('assets'));
 // use express router 
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log("There's an error in listening");
